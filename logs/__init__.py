2845e475f0d15d59f9b4446be99089bfcccf6fef
Simon Zimmermann <simonz05@gmail.com>
2013-05-27 11:28:46 +0200

    0.2.0 -> 0.2.1

--- logs/__init__.py
+++ logs/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 from .log import Parser, Analyzer
 from .fmt import fmt
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __all__ = ['Analyzer', 'Parser', 'fmt']
